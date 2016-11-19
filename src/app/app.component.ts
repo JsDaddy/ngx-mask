185d4fb1c0049543531a71f85d54d9c8831f4afc
Nepipenko Igor <igornepipenko@gmail.com>
2016-11-20 01:20:28 +0200

    add pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  masks = ['00/00/0000',
-  '00:00:00',
-  '00/00/0000 00:00:00',
-  '00000-000',
-  '0000-0000',
-  '(00) 0000-0000',
-  '+0(000) 000-0000',
-  '(000) (000) - 0000',
-  '000.000.000-00']
-
-  public someValue:string[] = ['ssds'];
-
-
-
 
 }
