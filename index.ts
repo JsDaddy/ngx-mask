570254b324d90eeaa49cade99ba14956dfca3f46
Stepan Suvorov <stevermeister@gmail.com>
2016-11-26 16:52:51 +0100

    added module to index

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-export * from './src/app/ng2-mask/ng2-mask.module'
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaskDirective } from './src/app/ng2-mask/mask.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports:[MaskDirective],
+  declarations: [MaskDirective]
+})
+export class Ng2MaskModule { }
