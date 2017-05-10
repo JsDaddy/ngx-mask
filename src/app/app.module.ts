fd8277f49adcc689fbc4c7416b361276101747ad
Victor Almeida Schinaider <victorschinaider@outlook.com>
2017-05-10 17:22:45 -0300

    updating material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdGridListModule, MdToolbarModule, MdIconModule, MdInputModule,
+  MdSelectModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import {Ng2MaskModule} from './ng2-mask/ng2-mask.module';
+import { Ng2MaskModule } from './ng2-mask/ng2-mask.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     ReactiveFormsModule,
+    MdGridListModule,
+    MdToolbarModule,
+    MdSelectModule,
+    MdInputModule,
     BrowserModule,
+    MdIconModule,
     FormsModule,
     HttpModule,
     Ng2MaskModule
