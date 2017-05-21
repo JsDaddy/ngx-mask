2533432966da4c04c087810f3e4fc9e7d36dac0f
igornepipenko <igornepipenko@gmail.com>
2017-05-21 20:50:10 +0300

    some tslint fix

--- src/app/ng2-mask/ng2-mask-options.ts
+++ src/app/ng2-mask/ng2-mask-options.ts
@@ -1,4 +1,4 @@
-export interface Ng2MaskOptions {
-  specialCharacters: Array<string>;
+export interface INg2MaskOptions {
+  specialCharacters: string[];
   patterns: { [key: string]: RegExp };
 };
