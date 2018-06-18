aa38e3d4621df90487e89a344d812cf0a72eb41e
sfs-inep <igornepipenko@gmail.com>
2018-06-18 15:55:44 +0300

    fix #125

--- src/app/ngx-mask/config.ts
+++ src/app/ngx-mask/config.ts
@@ -2,6 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 export interface IConfig {
   clearIfNotMatch: boolean;
+  showTemplate: boolean;
   dropSpecialCharacters: boolean | string[];
   specialCharacters: string[];
   patterns: {
@@ -12,16 +13,14 @@ export interface IConfig {
   };
 }
 
-export type optionsConfig = {
-  [P in keyof IConfig]?: IConfig[P]
-};
-
+export type optionsConfig = Partial<IConfig>;
 export const config: InjectionToken<string> = new InjectionToken('config');
 export const NEW_CONFIG: InjectionToken<string> = new InjectionToken('NEW_CONFIG');
 export const INITIAL_CONFIG: InjectionToken<IConfig> = new InjectionToken('INITIAL_CONFIG');
 
 export const initialConfig: IConfig = {
   clearIfNotMatch: false,
+  showTemplate: false,
   dropSpecialCharacters: true,
   specialCharacters: ['/', '(', ')', '.', ':', '-', ' ', '+', ',', '@'],
   patterns: {
