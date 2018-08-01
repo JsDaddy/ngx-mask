a7fbda1b48644d93cef3f6a8991926ab5a9506cb
Eduardo Cancino <ecancino@users.noreply.github.com>
2018-08-01 10:35:08 -0400

    Share MaskPipe (#158)

--- src/public_api.ts
+++ src/public_api.ts
@@ -2,3 +2,4 @@ export * from './app/ngx-mask/config';
 export * from './app/ngx-mask/mask.directive';
 export * from './app/ngx-mask/mask.service';
 export * from './app/ngx-mask/ngx-mask.module';
+export * from './app/ngx-mask/mask.pipe';
