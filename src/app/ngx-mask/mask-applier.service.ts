bb52901262c8bec77f64e47667f831ee9a99c900
Thegrep01 <38587958+Thegrep01@users.noreply.github.com>
2018-12-11 17:08:10 +0200

    Feature: Added new mask that separates number by thousands [ref: #246] (#257)

--- src/app/ngx-mask/mask-applier.service.ts
+++ src/app/ngx-mask/mask-applier.service.ts
@@ -34,16 +34,16 @@ export class MaskApplierService {
     }
     // tslint:disable-next-line:no-any
     public applyMaskWithPattern(inputValue: string, maskAndPattern: [string, IConfig['patterns']]): string {
-      const [mask, customPattern] = maskAndPattern;
-      this.customPattern = customPattern;
-      return this.applyMask(inputValue, mask);
+        const [mask, customPattern] = maskAndPattern;
+        this.customPattern = customPattern;
+        return this.applyMask(inputValue, mask);
     }
     public applyMask(
         inputValue: string,
         maskExpression: string,
         position: number = 0,
-        cb: Function = () => {}
-      ): string {
+        cb: Function = () => { }
+    ): string {
         if (inputValue === undefined || inputValue === null || maskExpression === undefined) {
             return '';
         }
@@ -59,92 +59,101 @@ export class MaskApplierService {
         const inputArray: string[] = inputValue.toString()
             .split('');
 
-        // tslint:disable-next-line
-        for (let i: number = 0, inputSymbol: string = inputArray[0]; i
-        < inputArray.length; i++, inputSymbol = inputArray[i]) {
-            if (cursor === maskExpression.length) {
-                break;
-            }
-            if (this._checkSymbolMask(inputSymbol, maskExpression[cursor]) && maskExpression[cursor + 1] === '?') {
-                result += inputSymbol;
-                cursor += 2;
-            } else if (
-              maskExpression[cursor + 1] === '*' && multi
-              && this._checkSymbolMask(inputSymbol, maskExpression[cursor + 2])
-            ) {
-              result += inputSymbol;
-              cursor += 3;
-              multi = false;
-            } else if (this._checkSymbolMask(inputSymbol, maskExpression[cursor])
-                && maskExpression[cursor + 1]
-                === '*') {
-                result += inputSymbol;
-                multi = true;
-            } else if (maskExpression[cursor + 1] === '?' && this._checkSymbolMask(
-                inputSymbol,
-                maskExpression[cursor + 2]
-            )) {
-                result += inputSymbol;
-                cursor += 3;
-            } else if (this._checkSymbolMask(inputSymbol, maskExpression[cursor])) {
-                if (maskExpression[cursor] === 'd') {
-                  if (Number(inputSymbol) > 3) {
-                    result += 0;
-                    cursor += 1;
+        if (maskExpression === 'separator') {
+            const strForSep: string = inputValue.replace(/\s/g, '');
+            result = this.separator(strForSep);
+            position = result.length + 1;
+            cursor = position;
+            const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
+                ? inputArray.length
+                : cursor;
+            this._shift.add(shiftStep + this.prefix.length || 0);
+        } else {
+            // tslint:disable-next-line
+            for (let i: number = 0, inputSymbol: string = inputArray[0]; i
+                < inputArray.length; i++ , inputSymbol = inputArray[i]) {
+                if (cursor === maskExpression.length) {
+                    break;
+                }
+                if (this._checkSymbolMask(inputSymbol, maskExpression[cursor]) && maskExpression[cursor + 1] === '?') {
+                    result += inputSymbol;
+                    cursor += 2;
+                } else if (
+                    maskExpression[cursor + 1] === '*' && multi
+                    && this._checkSymbolMask(inputSymbol, maskExpression[cursor + 2])
+                ) {
+                    result += inputSymbol;
+                    cursor += 3;
+                    multi = false;
+                } else if (this._checkSymbolMask(inputSymbol, maskExpression[cursor])
+                    && maskExpression[cursor + 1]
+                    === '*') {
+                    result += inputSymbol;
+                    multi = true;
+                } else if (maskExpression[cursor + 1] === '?' && this._checkSymbolMask(
+                    inputSymbol,
+                    maskExpression[cursor + 2]
+                )) {
+                    result += inputSymbol;
+                    cursor += 3;
+                } else if (this._checkSymbolMask(inputSymbol, maskExpression[cursor])) {
+                    if (maskExpression[cursor] === 'd') {
+                        if (Number(inputSymbol) > 3) {
+                            result += 0;
+                            cursor += 1;
+                            const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
+                                ? inputArray.length
+                                : cursor;
+                            this._shift.add(shiftStep + this.prefix.length || 0);
+                            i--;
+                            continue;
+                        }
+                    }
+                    if (maskExpression[cursor - 1] === 'd') {
+                        if (Number(inputValue.slice(cursor - 1, cursor + 1)) > 31) {
+                            continue;
+                        }
+                    }
+                    if (maskExpression[cursor] === 'm') {
+                        if (Number(inputSymbol) > 1) {
+                            result += 0;
+                            cursor += 1;
+                            const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
+                                ? inputArray.length
+                                : cursor;
+                            this._shift.add(shiftStep + this.prefix.length || 0);
+                            i--;
+                            continue;
+                        }
+                    }
+                    if (maskExpression[cursor - 1] === 'm') {
+                        if (Number(inputValue.slice(cursor - 1, cursor + 1)) > 12) {
+                            continue;
+                        }
+                    }
+                    result += inputSymbol;
+                    cursor++;
+                } else if (this.maskSpecialCharacters.indexOf(maskExpression[cursor]) !== -1) {
+                    result += maskExpression[cursor];
+                    cursor++;
                     const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
-                      ? inputArray.length
-                      : cursor;
-                    this._shift.add( shiftStep + this.prefix.length || 0);
+                        ? inputArray.length
+                        : cursor;
+                    this._shift.add(shiftStep + this.prefix.length || 0);
                     i--;
-                    continue;
-                  }
+                } else if (this.maskSpecialCharacters.indexOf(inputSymbol) > -1
+                    && this.maskAvailablePatterns[maskExpression[cursor]]
+                    && this.maskAvailablePatterns[maskExpression[cursor]].optional) {
+                    cursor++;
+                    i--;
+                } else if ((this.maskExpression[cursor + 1] === '*')
+                    && (this._findSpecialChar(this.maskExpression[cursor + 2]))
+                    && (this._findSpecialChar(inputSymbol) === this.maskExpression[cursor + 2]) && multi) {
+                    cursor += 3;
+                    result += inputSymbol;
                 }
-              if (maskExpression[cursor - 1] === 'd') {
-                  if (Number(inputValue.slice(cursor - 1, cursor + 1)) > 31 ) {
-                    continue;
-                  }
-              }
-              if (maskExpression[cursor] === 'm') {
-                if (Number(inputSymbol) > 1) {
-                  result += 0;
-                  cursor += 1;
-                  const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
-                    ? inputArray.length
-                    : cursor;
-                  this._shift.add( shiftStep + this.prefix.length || 0);
-                  i--;
-                  continue;
-                }
-              }
-              if (maskExpression[cursor - 1] === 'm') {
-                if (Number(inputValue.slice(cursor - 1, cursor + 1)) > 12 ) {
-                  continue;
-                }
-              }
-                result += inputSymbol;
-                cursor++;
-            } else if (this.maskSpecialCharacters.indexOf(maskExpression[cursor]) !== -1) {
-                result += maskExpression[cursor];
-                cursor++;
-                const shiftStep: number = /\*|\?/g.test(maskExpression.slice(0, cursor))
-                    ? inputArray.length
-                    : cursor;
-                this._shift.add(shiftStep + this.prefix.length || 0);
-                i--;
-            } else if (this.maskSpecialCharacters.indexOf(inputSymbol) > -1
-                && this.maskAvailablePatterns[maskExpression[cursor]]
-                && this.maskAvailablePatterns[maskExpression[cursor]].optional) {
-                cursor++;
-                i--;
-            } else if ( (this.maskExpression[cursor + 1] === '*')
-              && (this._findSpecialChar(this.maskExpression[cursor + 2]))
-              && (this._findSpecialChar(inputSymbol) === this.maskExpression[cursor + 2]) && multi ) {
-              cursor += 3;
-              result += inputSymbol;
             }
         }
-
-
         if (result.length + 1 === maskExpression.length
             && this.maskSpecialCharacters.indexOf(maskExpression[maskExpression.length - 1]) !== -1) {
             result += maskExpression[maskExpression.length - 1];
@@ -167,18 +176,30 @@ export class MaskApplierService {
         }
         return res;
     }
-    public _findSpecialChar (inputSymbol: string): undefined | string {
-      const symbol: string | undefined = this.maskSpecialCharacters
-          .find( (val: string) => val === inputSymbol);
-      return symbol ;
+    public _findSpecialChar(inputSymbol: string): undefined | string {
+        const symbol: string | undefined = this.maskSpecialCharacters
+            .find((val: string) => val === inputSymbol);
+        return symbol;
     }
 
     private _checkSymbolMask(inputSymbol: string, maskSymbol: string): boolean {
-      this.maskAvailablePatterns = this.customPattern
-        ? this.customPattern
-        : this.maskAvailablePatterns;
+        this.maskAvailablePatterns = this.customPattern
+            ? this.customPattern
+            : this.maskAvailablePatterns;
         return this.maskAvailablePatterns[maskSymbol]
             && this.maskAvailablePatterns[maskSymbol].pattern
             && this.maskAvailablePatterns[maskSymbol].pattern.test(inputSymbol);
     }
+
+    private separator = (str: string) => {
+        str += '';
+        const x: string[] = str.split(' ');
+        let res: string = x[0];
+        const rgx: RegExp = /(\d+)(\d{3})/;
+        while (rgx.test(res)) {
+            res = res.replace(rgx, '$1' + ' ' + '$2');
+        }
+        return res;
+    }
 }
+
---
+++ src/app/ngx-mask/test/separator.spec.ts
@@ -0,0 +1,52 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxMaskModule } from '../ngx-mask.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TestMaskComponent } from './utils/test-component.component';
+import { equal } from './utils/test-functions.component';
+
+
+describe('Separator: Mask', () => {
+
+  let fixture: ComponentFixture<TestMaskComponent>;
+  let component: TestMaskComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestMaskComponent],
+      imports: [ReactiveFormsModule, NgxMaskModule.forRoot()]
+    });
+    fixture = TestBed.createComponent(TestMaskComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('separator for empty', () => {
+    component.mask = 'separator';
+    equal('', '', fixture);
+  });
+
+  it('separator for 100', () => {
+    component.mask = 'separator';
+    equal('100', '100', fixture);
+  });
+
+  it('separator for 1000', () => {
+    component.mask = 'separator';
+    equal('1000', '1 000', fixture);
+  });
+
+  it('separator for 10000', () => {
+    component.mask = 'separator';
+    equal('10000', '10 000', fixture);
+  });
+
+  it('separator for 100000', () => {
+    component.mask = 'separator';
+    equal('100000', '100 000', fixture);
+  });
+
+  it('separator for 1000000', () => {
+    component.mask = 'separator';
+    equal('1000000', '1 000 000', fixture);
+  });
+});
