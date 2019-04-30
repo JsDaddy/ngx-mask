22ad86c349ca21f0f9a1ddb9a683edaaea71089a
Sergio <user@Users-MacBook-Pro-2.local>
2019-04-30 20:07:07 +0300

    prefix

--- src/app/ngx-mask/mask.service.ts
+++ src/app/ngx-mask/mask.service.ts
@@ -174,7 +174,8 @@ export class MaskService extends MaskApplierService {
     }
 
     public clearIfNotMatchFn(): void {
-        if (this.clearIfNotMatch === true && this.maskExpression.length !== this._formElement.value.length) {
+        if (this.clearIfNotMatch === true &&
+            this.prefix.length + this.maskExpression.length + this.sufix.length !== this._formElement.value.length) {
             this.formElementProperty = ['value', ''];
             this.applyMask(this._formElement.value, this.maskExpression);
         }
