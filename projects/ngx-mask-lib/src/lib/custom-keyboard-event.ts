b07d35051ca93bef53b610efbe6020d2154ebeaa
Ihor <igornepipenko@gmail.com>
2020-09-29 11:42:42 +0300

    fix: prettier check [ref:no-ref]

--- projects/ngx-mask-lib/src/lib/custom-keyboard-event.ts
+++ projects/ngx-mask-lib/src/lib/custom-keyboard-event.ts
@@ -2,16 +2,20 @@
 declare var global: any;
 
 const commonjsGlobal =
-    typeof globalThis !== 'undefined' ? globalThis :
-        typeof window !== 'undefined' ? window :
-            typeof global !== 'undefined' ? global :
-                typeof self !== 'undefined' ? self :
-                    {};
+  typeof globalThis !== 'undefined'
+    ? globalThis
+    : typeof window !== 'undefined'
+    ? window
+    : typeof global !== 'undefined'
+    ? global
+    : typeof self !== 'undefined'
+    ? self
+    : {};
 
-(function() {
-    if (!commonjsGlobal.KeyboardEvent) {
-        commonjsGlobal.KeyboardEvent = function(_eventType: any, _init: any) { };
-    }
+(function () {
+  if (!commonjsGlobal.KeyboardEvent) {
+    commonjsGlobal.KeyboardEvent = function (_eventType: any, _init: any) {};
+  }
 })();
 
 export type CustomKeyboardEvent = KeyboardEvent;
