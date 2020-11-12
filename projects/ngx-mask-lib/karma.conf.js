1792f7330ca1a8fbabc7db16f3d7b8aa015d5c6e
Vladimir Loban <juncker8888@gmail.com>
2020-11-12 15:32:23 +0200

    feat(mask-applier): migrate to angular 11

--- projects/ngx-mask-lib/karma.conf.js
+++ projects/ngx-mask-lib/karma.conf.js
@@ -11,35 +11,18 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-mocha-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../test-reports/coverage/ngx-mask-lib'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
-        // thresholds for all files
-        global: {
-          statements: 80,
-          lines: 80,
-          branches: 70,
-          functions: 80,
-        },
-        // thresholds per file
-        each: {
-          statements: 68,
-          lines: 64,
-          branches: 35,
-          functions: 45,
-        },
-      },
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+      reporters: [{ type: 'html' }, { type: 'lcov' }],
     },
-    reporters: ['mocha', 'kjhtml'],
 
     // Config for the karma-mocha-reporter
     // https://www.npmjs.com/package/karma-mocha-reporter
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -11,40 +11,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-mocha-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../test-reports/coverage/showcase'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        emitWarning: false, // set to `true` to not fail the test command when thresholds are not met
-        // thresholds for all files
-        global: {
-          statements: 5,
-          lines: 5,
-          branches: 5,
-          functions: 5,
-        },
-        // thresholds per file
-        each: {
-          statements: 0,
-          lines: 0,
-          branches: 0,
-          functions: 0,
-        },
-      },
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+      reporters: [{ type: 'html' }, { type: 'lcov' }],
     },
-    reporters: ['mocha', 'kjhtml'],
 
     // Config for the karma-mocha-reporter
     // https://www.npmjs.com/package/karma-mocha-reporter
     mochaReporter: {
-      // Ignore skipped from the output, very ueful when using fdescribe and only want the one test/class showing up
+      // Ignore skipped from the output, very useful when using fdescribe and only want the one test/class showing up
       ignoreSkipped: true,
     },
 
