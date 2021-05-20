ac298c086f332a94ca2b45e1483407980f161c12
Manuel Meister <manuel.meister@postfinance.ch>
2021-05-20 14:54:15 +0200

    test(directive): add delete e2e tests

---
+++ projects/ngx-mask-lib/src/test/utils/cypress-test-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, NgModule } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { NgxMaskModule } from 'ngx-mask';
+
+@Component({
+  selector: 'mask-cypress-test-mask',
+  template: ` <input id="maska" [formControl]="form" [mask]="mask" [hiddenInput]="hiddenInput" [prefix]="prefix" /> `,
+})
+export class CypressTestMaskComponent {
+  @Input() public mask!: string | null;
+  @Input() public hiddenInput: boolean = false;
+  @Input() public prefix: string = '';
+  public form: FormControl = new FormControl('');
+}
+
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, NgxMaskModule.forRoot()],
+  declarations: [CypressTestMaskComponent],
+  exports: [CypressTestMaskComponent],
+})
+export class CypressTestMaskModule {}
