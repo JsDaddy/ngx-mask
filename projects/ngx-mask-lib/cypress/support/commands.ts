7e50bdc221b0766c85dd9c116300960644945335
Manuel Meister <manuel@meister.id>
2021-12-30 16:51:24 +0100

    fix(tests): enable cypress component tests
    Signed-off-by: Manuel Meister <manuel@meister.id>

--- projects/ngx-mask-lib/cypress/support/commands.ts
+++ projects/ngx-mask-lib/cypress/support/commands.ts
@@ -1,12 +1,70 @@
 declare namespace Cypress {
 	// eslint-disable-next-line @typescript-eslint/naming-convention
 	interface Chainable {
-		setSelectionRange(start: number, end: number): Chainable<JQuery<HTMLInputElement>>;
+		setSelectionRange(
+			start: number,
+			end: number,
+			options?: Partial<Loggable & Timeoutable>,
+		): Chainable<JQuery<HTMLInputElement>>;
 	}
 }
 
-Cypress.Commands.add('setSelectionRange', { prevSubject: 'element' }, (subject, start, end) => {
-	return cy.wrap(subject).then(($el: JQuery<HTMLInputElement>) => {
-		$el[0]?.setSelectionRange(start, end);
-	});
-});
+Cypress.Commands.add(
+	'setSelectionRange',
+	{ prevSubject: 'element' },
+	// @ts-ignore
+	(subject: Chainable<JQuery<HTMLInputElement>>, start: number, end: number, userOptions?: any) => {
+		let options = {
+			$el: subject,
+			error: true,
+			log: true,
+			verify: true,
+			...userOptions,
+		};
+
+		if (options.log) {
+			options._log = Cypress.log({
+				$el: options.$el,
+				timeout: options.timeout,
+				consoleProps() {
+					return { 'Applied To': Cypress.dom.getElements(options.$el) };
+				},
+			});
+		}
+
+		const el = options.$el[0];
+
+		if (
+			!(
+				el.tagName == 'TEXTAREA' ||
+				(el.tagName == 'INPUT' &&
+					/^(text|search|URL|tel|password|null)$/.test(el.getAttribute('type')))
+			)
+		) {
+			if (!options.error) {
+				return;
+			}
+
+			throw new Error('setSelectionRange.invalid_element');
+		}
+
+		if (options.$el.length && options.$el.length > 1) {
+			if (!options.error) {
+				return;
+			}
+
+			throw new Error('setSelectionRange.multiple_elements');
+		}
+
+		el.setSelectionRange(start, end);
+
+		const verifyAssertions = () => {
+			// @ts-ignore
+			return cy.verifyUpcomingAssertions(options.$el, options, {
+				onRetry: verifyAssertions,
+			});
+		};
+
+		return verifyAssertions();
+	},
+);
