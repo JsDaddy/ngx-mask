d79563599eddfce2becd9614c970e5128b43ec75
Manuel Meister <news.manuelsworld@gmail.com>
2022-01-11 14:38:06 +0100

    Feature: Enable non zero selection (#964)
    * fix(tests): enable cypress component tests
    
    Signed-off-by: Manuel Meister <manuel@meister.id>
    
    * fix(lib): enable deletion of non-zero-selections
    
    * chore(lib): update version and add changelog
    
    Co-authored-by: Manuel Meister <manuel@meister.id>

--- projects/ngx-mask-lib/cypress/plugins/index.ts
+++ projects/ngx-mask-lib/cypress/plugins/index.ts
@@ -1,11 +1,8 @@
-import { ResolvedDevServerConfig } from '@cypress/webpack-dev-server';
-import { startAngularDevServer } from '@jscutlery/cypress-angular-dev-server';
+import { startAngularDevServer } from '@jscutlery/cypress-angular';
 
-module.exports = (
-	on: (arg0: string, arg1: (options: any) => Promise<ResolvedDevServerConfig>) => void,
-	config: any,
-) => {
-	on('dev-server:start', (options) =>
+// @ts-ignore
+module.exports = (on, config) => {
+	on('dev-server:start', (options: any) =>
 		startAngularDevServer({ options, tsConfig: 'tsconfig.cypress.json' }),
 	);
 	return config;
--- projects/ngx-mask-lib/src/lib/mask.directive.ts
+++ projects/ngx-mask-lib/src/lib/mask.directive.ts
@@ -354,7 +354,7 @@ export class MaskDirective implements ControlValueAccessor, OnChanges, Validator
 	}
 
 	@HostListener('click', ['$event'])
-	public onFocus(e: MouseEvent | CustomKeyboardEvent): void {
+	public onClick(e: MouseEvent | CustomKeyboardEvent): void {
 		if (!this._maskValue) {
 			return;
 		}
@@ -441,7 +441,7 @@ export class MaskDirective implements ControlValueAccessor, OnChanges, Validator
 					? this.specialCharacters
 					: this._config.specialCharacters;
 				if (this.prefix.length > 1 && (el.selectionStart as number) <= this.prefix.length) {
-					el.setSelectionRange(this.prefix.length, this.prefix.length);
+					el.setSelectionRange(this.prefix.length, el.selectionEnd);
 				} else {
 					if (
 						this._inputValue.length !== (el.selectionStart as number) &&
@@ -454,16 +454,12 @@ export class MaskDirective implements ControlValueAccessor, OnChanges, Validator
 							((this.prefix.length >= 1 && (el.selectionStart as number) > this.prefix.length) ||
 								this.prefix.length === 0)
 						) {
-							el.setSelectionRange(
-								(el.selectionStart as number) - 1,
-								(el.selectionStart as number) - 1,
-							);
+							el.setSelectionRange((el.selectionStart as number) - 1, el.selectionEnd);
 						}
 					}
-					this.suffixCheckOnPressDelete(e.keyCode, el);
 				}
 			}
-			this.suffixCheckOnPressDelete(e.keyCode, el);
+			this.checkSelectionOnDeletion(el);
 			if (
 				this._maskService.prefix.length &&
 				(el.selectionStart as number) <= this._maskService.prefix.length &&
@@ -561,23 +557,15 @@ export class MaskDirective implements ControlValueAccessor, OnChanges, Validator
 		this.onTouch = fn;
 	}
 
-	public suffixCheckOnPressDelete(keyCode: number, el: HTMLInputElement): void {
-		if (keyCode === 46 && this.suffix.length > 0) {
-			if (this._inputValue.length - this.suffix.length <= (el.selectionStart as number)) {
-				el.setSelectionRange(this._inputValue.length - this.suffix.length, this._inputValue.length);
-			}
-		}
-		if (keyCode === 8) {
-			if (
-				this.suffix.length > 1 &&
-				this._inputValue.length - this.suffix.length < (el.selectionStart as number)
-			) {
-				el.setSelectionRange(this._inputValue.length - this.suffix.length, this._inputValue.length);
-			}
-			if (this.suffix.length === 1 && this._inputValue.length === (el.selectionStart as number)) {
-				el.setSelectionRange((el.selectionStart as number) - 1, (el.selectionStart as number) - 1);
-			}
-		}
+	public checkSelectionOnDeletion(el: HTMLInputElement): void {
+		el.selectionStart = Math.min(
+			Math.max(this.prefix.length, el.selectionStart as number),
+			this._inputValue.length - this.suffix.length,
+		);
+		el.selectionEnd = Math.min(
+			Math.max(this.prefix.length, el.selectionEnd as number),
+			this._inputValue.length - this.suffix.length,
+		);
 	}
 
 	/** It disables the input element */
