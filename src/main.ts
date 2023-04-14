20192ef1692a346d9b14f366631db09cafbcb3eb
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-14 19:05:24 +0300

    fix(no-ref): fix import

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import { IConfig, provideNgxMask } from 'ngx-mask';
 import { provideRouter } from '@angular/router';
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { provideHttpClient } from '@angular/common/http';
-import {BaseHttpService} from "@libraries/base-http/base-http.service";
-import {DOMAIN} from "@libraries/token/token";
-import { GithubStarsService } from '@libraries/github-stars/github-stars.service';
+import { BaseHttpService } from '@libraries/base-http/base-http.service';
+import { DOMAIN } from '@libraries/token/token';
+import { GithubStarsService } from '@libraries/github/github-stars.service';
 
 if (environment.production) {
     enableProdMode();
