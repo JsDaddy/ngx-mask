9a7733f06c8063807031896713a07e40d0932242
Igor Nepipenko <inepipenko@muellerone.com>
2023-04-23 23:06:58 +0200

    fix(no-ref): close non public api

--- projects/ngx-mask-lib/src/index.ts
+++ projects/ngx-mask-lib/src/index.ts
@@ -2,6 +2,4 @@ export * from './lib/ngx-mask.providers';
 export * from './lib/ngx-mask.directive';
 export * from './lib/ngx-mask.pipe';
 export * from './lib/ngx-mask.service';
-export * from './lib/ngx-mask-applier.service';
 export * from './lib/ngx-mask.config';
-export * from './lib/custom-keyboard-event';
