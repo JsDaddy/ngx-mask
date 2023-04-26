d6913dc2dd8b54c092565d52f60e70f34be341d8
andriikamaldinov1 <akam@jsdaddy.io>
2023-04-26 12:31:29 +0300

    feat(no-ref): redisign

--- src/app/shared/service/change-accordion.service.ts
+++ src/app/shared/service/change-accordion.service.ts
@@ -1,17 +1,11 @@
-import { ElementRef, inject, Injectable, QueryList } from '@angular/core';
-import { Router } from '@angular/router';
+import { ElementRef, Injectable, QueryList } from '@angular/core';
 import { takeUntil } from 'rxjs';
 import { UnSubscriber } from '@libraries/unsubscriber/unsubscriber.service';
 
 @Injectable()
 export class ChangeAccordionService extends UnSubscriber {
-    private readonly router = inject(Router);
-
     public onChangeAccordion(cards: QueryList<ElementRef>): void {
         cards.changes.pipe(takeUntil(this.unsubscribe$$)).subscribe((elementRef) => {
-            this.router.navigate(['/'], {
-                fragment: '1',
-            });
             const firstNativeElement: HTMLElement | null = document.getElementById(
                 elementRef.first.nativeElement.id
             );
