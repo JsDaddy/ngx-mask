f5c6e989cf6fa99f024c7958f2329d5c6c4b016c
andriikamaldinov1 <akam@jsdaddy.io>
2023-05-26 14:54:15 +0300

    fix(#1018): fix 1018

--- projects/ngx-mask-lib/src/test/utils/cypress-test-component.component.ts
+++ projects/ngx-mask-lib/src/test/utils/cypress-test-component.component.ts
@@ -4,13 +4,20 @@ import { FormControl } from '@angular/forms';
 @Component({
     selector: 'jsdaddy-open-source-test',
     template: `
-        <input id="masked" [formControl]="form" [mask]="mask" [hiddenInput]="hiddenInput" />
+        <input
+            id="masked"
+            [formControl]="form"
+            [mask]="mask"
+            [prefix]="prefix"
+            [hiddenInput]="hiddenInput" />
     `,
 })
 export class CypressTestMaskComponent {
     @Input() public mask = '';
 
     @Input() public hiddenInput = false;
+
+    @Input() public prefix = '';
 
     public form: FormControl = new FormControl('');
 }
