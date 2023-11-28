5d08bc0d9a04e92ce4c8cd274a44048b10d9924a
andriikamaldinov1 <akam@jsdaddy.io>
2023-11-28 12:13:11 +0200

    fix(ref: 1271): fix 1271

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-// import { provideNgxMask } from 'ngx-mask';
-import { IConfig, initialConfig, provideNgxMask } from 'ngx-mask';
+import { provideNgxMask } from 'ngx-mask';
 import { provideRouter } from '@angular/router';
 import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 import { provideHttpClient } from '@angular/common/http';
@@ -16,18 +15,6 @@ import { GithubStarsService } from '@libraries/github/github-stars.service';
 if (environment.production) {
     enableProdMode();
 }
-const ngxMaskConfig: IConfig = {
-    ...initialConfig,
-    patterns: {
-        ...initialConfig.patterns,
-        f: {
-            pattern: /[a-zA-Z0-9 ]/,
-        },
-        F: {
-            pattern: /[а-яА-Яa-zA-Z0-9 ]/,
-        },
-    },
-};
 
 bootstrapApplication(AppComponent, {
     providers: [
@@ -40,8 +27,7 @@ bootstrapApplication(AppComponent, {
         BaseHttpService,
         provideAnimations(),
         provideRouter([]),
-        // provideNgxMask(),
-        provideNgxMask(ngxMaskConfig),
+        provideNgxMask(),
         {
             provide: HIGHLIGHT_OPTIONS,
             useValue: {
