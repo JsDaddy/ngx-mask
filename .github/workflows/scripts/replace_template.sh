13d5134b56abbfdefe6039833c61a15690fb00bc
andriikamaldinov1 <akam@jsdaddy.io>
2024-03-18 19:29:15 +0200

    fix(ref: no-ref): fix path in replace script

--- .github/workflows/scripts/replace_template.sh
+++ .github/workflows/scripts/replace_template.sh
@@ -8,10 +8,9 @@ fi
 
 current_directory=$(pwd)
 echo "Current directory: $current_directory"
-ls $current_directory
 # Assign arguments to variables
 custom_string="$1"
 
 # Perform the replacement and save to output file
-sed -i "" "s/<%version%>/$custom_string/g" "angular.json"
+sed -i "s/<%version%>/$custom_string/g" "angular.json"
 echo "Template string replaced successfully.  🎉"
