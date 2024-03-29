fd650e394fd81cd5baf24ea7dff51c4ab82c95e0
andriikamaldinov1 <akam@jsdaddy.io>
2024-03-29 12:32:49 +0200

    feat(ref: no-ref): remove environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { enableProdMode, ɵprovideZonelessChangeDetection } from '@angular/core';
+import { ɵprovideZonelessChangeDetection } from '@angular/core';
 
-import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -11,10 +10,6 @@ import { provideHttpClient } from '@angular/common/http';
 import { BaseHttpService } from '@libraries/base-http/base-http.service';
 import { DOMAIN } from '@libraries/token/token';
 import { GithubStarsService } from '@libraries/github/github-stars.service';
-
-if (environment.production) {
-    enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
     providers: [
