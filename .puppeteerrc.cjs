d9d4a20b035e68614a1171338284a174b70b2952
andriikamaldinov1 <akam@jsdaddy.io>
2024-10-22 19:05:11 +0300

    fix(ref: no-ref): fix view, remove extra divs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,4 +1,4 @@
-const {join} = require('path');
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
--- eslint.config.js
+++ eslint.config.js
@@ -24,7 +24,7 @@ const ignores = [
     'coverage-ts/',
     'cypress/',
     'package-lock.json',
-    '.cache'
+    '.cache',
 ];
 
 // Export our config array, which is composed together thanks to the typed utility function from typescript-eslint
