d7faab368c114d688cfa2c6502d8f040f05b9f73
andriikamaldinov1 <akam@jsdaddy.io>
2024-11-01 18:23:14 +0200

    fix(ref: 1440): add version lib version to slack

--- .github/workflows/scripts/quality.sh
+++ .github/workflows/scripts/quality.sh
@@ -7,16 +7,16 @@ bun lint
 # run snyk:auth -- $snyk_token
 # bun run snyk:test
 
-#output=$(bun run type-coverage)
-#if echo "$output" | grep -q "lower than "; then
-#    echo "$output"
-#    exit 1  # Terminate the hook script with a non-zero exit code
-#else
-#    echo "Type coverage is good! 🎉"
-#fi
-#
-#bun run test
-#bun run cypress:bash
+output=$(bun run type-coverage)
+if echo "$output" | grep -q "lower than "; then
+    echo "$output"
+    exit 1  # Terminate the hook script with a non-zero exit code
+else
+    echo "Type coverage is good! 🎉"
+fi
+
+bun run test
+bun run cypress:bash
 
 bun run build
 
