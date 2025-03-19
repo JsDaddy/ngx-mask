b3c218a66422b8075e702f6ec0a5828a9f9891aa
dmaza <david.maza@transport.wa.gov.au>
2025-03-19 15:07:57 +0800

    fix(forms): Ensure that the masked value is propagated to the form control if setValue() is called multiple times with values that are subsequently masked.

--- projects/ngx-mask-lib/src/test/forms.spec.ts
+++ projects/ngx-mask-lib/src/test/forms.spec.ts
@@ -39,4 +39,53 @@ describe('Directive: Forms', () => {
 
         component.form.setValue('A1234Z');
     });
+
+    it('should mask values when multiple calls to setValue() are made', () => {
+        component.form.setValue('A1234Z');
+        expect(component.form.value).toBe('1234');
+        component.form.setValue('A1234Z');
+        expect(component.form.value).toBe('1234');
+        component.form.setValue('A1234Z');
+        expect(component.form.value).toBe('1234');
+    });
+
+    it('should propagate masked value to the form control valueChanges observable when multiple calls to setValue() are made', () => {
+        component.form.valueChanges.subscribe((newValue) => {
+            expect(newValue).toEqual('1234');
+        });
+
+        component.form.setValue('A1234Z');
+        component.form.setValue('A1234Z');
+        component.form.setValue('A1234Z');
+    });
+
+    it('should not emit to valueChanges if the masked value has not changed with emitEvent: true', () => {
+        let emissionsToValueChanges = 0;
+
+        component.form.valueChanges.subscribe(() => {
+            emissionsToValueChanges++;
+        });
+
+        component.form.setValue('1234', { emitEvent: true });
+        component.form.setValue('1234', { emitEvent: true });
+
+        // Expect to emit 3 times, once for the first setValue() call, once by ngx-mask, and once for the second setValue() call.
+        // There is not fourth emission for when ngx-mask masks the value for a second time.
+        expect(emissionsToValueChanges).toBe(3);
+    });
+
+    it('should not emit to valueChanges if the masked value has not changed with emitEvent: false', () => {
+        let emissionsToValueChanges = 0;
+
+        component.form.valueChanges.subscribe(() => {
+            emissionsToValueChanges++;
+        });
+
+        component.form.setValue('1234', { emitEvent: false });
+        component.form.setValue('1234', { emitEvent: false });
+
+        // Expect to only have emitted once, only by ngx-mask.
+        // There is no second emission for when ngx-mask masks the value for a second time.
+        expect(emissionsToValueChanges).toBe(1);
+    });
 });
